9b9b135dacfc4a5704ebf994efc99a5abc0d3b84
mingxiao <52912517+Jmingxiao@users.noreply.github.com>
2023-06-02 19:43:42 +0200

    sth random

---
+++ UE5Expr/Source/UE5Expr/UE5Expr.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UE5Expr.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UE5Expr, "UE5Expr" );
---
+++ UE5Expr/Source/UE5Expr/UE5ExprGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UE5ExprGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE5EXPR_API AUE5ExprGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
